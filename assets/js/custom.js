51eb24364ebf7d4add43185c1d3594bba5995f8d
Abdul Manan <90985527+mananabbasi51325@users.noreply.github.com>
2023-12-28 19:29:01 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -362,3 +362,11 @@ function initLoginForm() {
 }
 
 initLoginForm();
+
+var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
+alert(x + ' × ' + y);
